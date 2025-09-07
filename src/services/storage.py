5f7615c2d38ae5d2c5666bc4e0d8136f6e9bb6db
AhmedEsammohmaed <141889285+AhmedEsammohmaed@users.noreply.github.com>
2025-09-08 00:23:01 +0300

    Update storage.py

--- src/services/storage.py
+++ src/services/storage.py
@@ -1,1 +1,17 @@
+import pickle
+from typing import Dict
+from src.models.user import User
+
+DATA_FILE = "user_balances.pkl"
+
+def save_users(users: Dict[int, User]):
+    with open(DATA_FILE, "wb") as f:
+        pickle.dump(users, f)
+
+def load_users() -> Dict[int, User]:
+    try:
+        with open(DATA_FILE, "rb") as f:
+            return pickle.load(f)
+    except (FileNotFoundError, EOFError):
+        return {}
 
